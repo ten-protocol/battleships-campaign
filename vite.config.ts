3e34514da6cddac5233d0f12c0aa8b17e6986986
Pete <p.bienek@gmail.com>
2024-08-21 16:44:08 +0100

    Add absolute paths to images

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import Checker from "vite-plugin-checker";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), Checker({ typescript: true })],
+  define: {
+    'import.meta.env.VITE_SITE_URL': `"https://${process.env.VERCEL_URL}"`,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
