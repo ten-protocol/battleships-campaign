774e8079c6a8380b4934c6a2a33cf217e7fe3cd5
Pete <p.bienek@gmail.com>
2024-08-21 16:56:58 +0100

    replace template tags

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 import path from 'path';
-import Checker from "vite-plugin-checker";
+import { defineConfig } from 'vite';
+import Checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Checker({ typescript: true })],
-  define: {
-    'import.meta.env.VITE_SITE_URL': `"https://${process.env.VERCEL_URL}"`,
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [
+        react(),
+        Checker({ typescript: true }),
+    ],
+    define: {
+        'import.meta.env.VITE_SITE_URL': process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : `""`,
     },
-  },
-})
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
+    },
+});
