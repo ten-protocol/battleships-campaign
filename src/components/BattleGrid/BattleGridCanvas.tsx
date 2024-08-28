ec9fde14ff5645da985e3655fda5ba4ec1d65174
Pete <p.bienek@gmail.com>
2024-08-28 14:35:35 +0100

    Reduces frame-rate and turns off anti-aliasing.

--- src/components/BattleGrid/BattleGridCanvas.tsx
+++ src/components/BattleGrid/BattleGridCanvas.tsx
@@ -9,16 +9,46 @@ import BattleGridExplosion from './BattleGridExplosion';
 import BattleGridHits from './BattleGridHits';
 import BattleGridMisses from './BattleGridMisses';
 import BattleGridUnknowns from './BattleGridUnknowns';
+import { useEffect, useState } from 'react';
+import { Application, ICanvas } from 'pixi.js';
+
 
 export default function BattleGridCanvas() {
+    const [app, setApp] = useState<Application<ICanvas>>();
+
+    useEffect(() => {
+        if (!app?.ticker) return;
+        app.ticker.autoStart = false;
+        app.ticker.stop();
+
+        const targetFPS = 30;
+        const targetInterval = 1000 / targetFPS;
+
+        let lastTime = performance.now();
+
+        const renderLoop = (currentTime: number) => {
+            const deltaTime = currentTime - lastTime;
+
+            if (deltaTime >= targetInterval) {
+                app.ticker.update(currentTime);
+                lastTime = currentTime - (deltaTime % targetInterval);
+            }
+
+            requestAnimationFrame(renderLoop);
+        };
+
+        requestAnimationFrame(renderLoop);
+    }, [app]);
+
     return (
         <Stage
+            onMount={(app) => setApp(app)}
             width={HEX_WIDTH * COLS + HEX_GRID_MARGIN * 1.5}
             height={HEX_HEIGHT * ROWS * 0.75 + HEX_GRID_MARGIN}
             options={{
                 backgroundAlpha: 0,
-                antialias: true,
-                resolution: 2,
+                antialias: false,
+                resolution: Math.floor(window.devicePixelRatio),
                 autoDensity: true,
             }}
         >
