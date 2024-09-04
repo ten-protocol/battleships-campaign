3540e698de558128dec42c79df2a517faf9645e8
Peter Bienek <p.bienek@gmail.com>
2024-09-04 15:28:15 +0100

    bump

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from 'vite';
 import Checker from 'vite-plugin-checker';
 import svgr from "vite-plugin-svgr";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
